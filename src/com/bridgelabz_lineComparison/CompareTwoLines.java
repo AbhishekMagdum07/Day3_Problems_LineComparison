02cb0ff9966ffdba68ce0b537a081f0370933a70
Abhishek <abhishek.magdum.07@gmail.com>
2023-02-21 19:14:38 +0530

    Added : Compare two lines using co-ordinates

--- src/com/bridgelabz_lineComparison/CompareTwoLines.java
+++ src/com/bridgelabz_lineComparison/CompareTwoLines.java
@@ -1,7 +1,7 @@
 package com.bridgelabz_lineComparison;
 
 import java.util.Scanner;
-public class LineComparison {
+public class CompareTwoLines {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Line 1 inputs :");
@@ -24,14 +24,15 @@ public class LineComparison {
         double q2 = sc.nextDouble();
 
 
-        // double length = Math.sqrt((x2 - x1)*(x2-x1) + (y2 - y1)*(y2-y1));
         double length1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
         double length2 = Math.sqrt(Math.pow(p2 - p1, 2) + Math.pow(q2 - q1, 2));
 
-        if(length1 == length2) {
+        if (length1 == length2) {
             System.out.println("Two Lines are Equal.");
-        }else {
-            System.out.println("Two Lines are not Equal.");
+        } else if (length1 > length2) {
+            System.out.println("Line 1 is greater than Line 2.");
+        } else {
+            System.out.println("Line 2 is greater than Line 1");
         }
     }
 }
